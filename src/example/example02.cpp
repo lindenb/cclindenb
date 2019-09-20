696900957874e9cea4fa6d3d847ebb01ef3a21cd
Fredrik Simonsson <simson@dds.co.jp>
2019-09-20 10:43:38 +0900

    Added a more specific example

---
+++ src/example/example02.cpp
@@ -0,0 +1,47 @@
+#include "tarball.h"
+#include <chrono>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  /* create the tar file */
+  try {
+    lindenb::io::Tar tarball(std::cout);
+    /* add item 1 */
+    tarball.put("myfiles/item1.txt", "Hello World 1\n");
+    /* add item 2 */
+    int i = 0;
+    while (true) {
+      std::stringstream hello;
+      hello << "Hello " << std::setfill('0') << std::setw(5) << i++
+            << " world\n";
+      tarball.put("item.txt", hello.str().c_str());
+      std::cout.flush();
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    try {
+      /* add a file */
+      tarball.putFile("tarfile.cpp", "myfiles/code.cpp");
+    } catch (std::runtime_error e) {
+      std::cerr << "Trying to add file failed\nError code from library::"
+                << e.what() << std::endl;
+    }
+
+    /* finalize the tar file */
+    tarball.finish();
+  } catch (std::runtime_error e) {
+    std::cerr << "tar lib errors\nError code from library::" << e.what()
+              << std::endl;
+  }
+  /* close the file */
+  // stdout.close();
+  /* we're done */
+  return EXIT_SUCCESS;
+  ;
+}
